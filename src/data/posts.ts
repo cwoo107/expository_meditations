ac9c846cc5d8262bcf8b621b77170c45db6524f7
cwoo107 <woodscaleb@gmail.com>
2024-12-03 15:28:35 -0700

    added col 1:15-23

--- src/data/posts.ts
+++ src/data/posts.ts
@@ -10,22 +10,28 @@ export const posts: Post[] = [
         title: "Psalm 149",
         date: "2024-11-20",
         slug: "psalm-149-reflection",
-        excerpt: "The Lord's people praise his name. Like children, the Lord's people dance and sing, making music to praise the Lord. Why?"
+        excerpt: "The Lord's people praise his name. Like children, the Lord's people dance and sing, making music to praise the Lord. Why?..."
     },
     {
         title: "Psalm 148",
         date: "2024-11-16",
         slug: "psalm-148-reflection",
-        excerpt: "All of creation sings praise to the Lord. 'For he commanded and they were created' (v5). It was by his own will, apart from any compulsion, that he created all things."
+        excerpt: "All of creation sings praise to the Lord. 'For he commanded and they were created' (v5). It was by his own will, apart from any compulsion..."
     },
     {
         title: "Psalm 147",
         date: "2024-11-14",
         slug: "psalm-147-reflection",
-        excerpt: "Praise the Lord! Who builds up his people, gathers the outcasts from around the world, heals the brokenhearted by binding up their wounds. He is 'abundant in power' that is he is all powerful."
+        excerpt: "Praise the Lord! Who builds up his people, gathers the outcasts from around the world, heals the brokenhearted by binding up their wounds..."
     },
     {
-        title: "Col 1:1-14",
+        title: "The Supremacy of Christ: Creator, Sustainer, and Reconciler (Col 1:15-23)",
+        date: "2024-03-30",
+        slug: "col-1-15-23-reflection",
+        excerpt: "Paul breaks out into a brief moment of worship highlighting the grandeur and supremacy of Christ. 'He is the image of the invisible God'..."
+    },
+    {
+        title: "The Supremacy of Christ and the Hope of the Gospel (Col 1:1-14)",
         date: "2024-03-25",
         slug: "col-1-1-14-reflection",
         excerpt: "Paul introduces his letter to the Colossian church first by signing his own name, then by introducing his title as an apostle of Christ Jesus. That is not an apostle in general, but of Christ specifically..."
